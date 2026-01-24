302f7195947e5990fc6b12353b17e1ff692e3aa6
nyimu tianhanly <130267348+tianhanly@users.noreply.github.com>
2026-01-25 00:28:12 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -107,14 +107,14 @@ export const siteConfig: SiteConfig = {
 				"/assets/desktop-banner/5.webp",
 				"/assets/desktop-banner/6.webp",
 			], // 桌面横幅图片
-			mobile: {
+			mobile: [
 				"/assets/mobile-banner/1.webp",
 				"/assets/mobile-banner/2.webp",
 				"/assets/mobile-banner/3.webp",
 				"/assets/mobile-banner/4.webp",
 				"/assets/mobile-banner/5.webp",
 				"/assets/mobile-banner/6.webp",
-			}, // 移动横幅图片
+			], // 移动横幅图片
 		}, // 使用本地横幅图片
 
 		position: "center", // 等同于 object-position，仅支持 'top', 'center', 'bottom'。默认为 'center'
